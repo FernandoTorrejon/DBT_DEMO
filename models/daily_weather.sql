31d6b701bb00817c4aad1035defb0f6bf47b484d
Fernando Lopez Perez <fernandolopez@paradigmadigital.com>
2025-07-09 16:59:02 +0000

    Daily weather model created

---
+++ models/daily_weather.sql
@@ -0,0 +1,31 @@
+WITH daily_weather as (
+    select 
+
+    date(time) as daily_weather,
+    weather,
+    temp,
+    pressure, 
+    humidity,
+    clouds
+
+    from {{ source('demo', 'weather') }}
+    limit 10
+),
+
+daily_weather_aggregate as (
+    select daily_weather,
+        weather,
+        round(avg(temp),2) as avg_temperature,
+        round(avg(pressure), 2) as avg_pressure, 
+        round(avg(humidity), 2) as avg_humidity,
+        round(avg(clouds), 2) as avg_clouds
+        --ROW_NUMBER() OVER (PARTITION BY daily_weather ORDER BY count(weather) DESC) as row_number
+    from daily_weather
+    group by daily_weather, weather
+
+    -- qualify is like a WHERE statement that you use after a GROUP BY statement
+    qualify ROW_NUMBER() OVER (PARTITION BY daily_weather ORDER BY count(weather) DESC) = 1
+
+)
+
+select * from daily_weather_aggregate
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -2,17 +2,11 @@ WITH CTE AS (
     select to_timestamp(started_at) as started_at, 
     date(to_timestamp(started_at)) as date_started_at,
     lpad(hour(to_timestamp(started_at)), 2, '0') as hour_started_at,
-    CASE
-      WHEN dayname(to_timestamp(started_at)) IN ('Sat','Sun') THEN 'Weekend' else 'BusinessDay' END as day_of_week,
-    CASE 
-      WHEN MONTH(to_timestamp(started_at)) IN (12,1,2) THEN 'Winter'
-      WHEN MONTH(to_timestamp(started_at)) IN (3,4,5) THEN 'Spring'
-      WHEN MONTH(to_timestamp(started_at)) IN (6,7,8) THEN 'Summer'
-      WHEN MONTH(to_timestamp(started_at)) IN (9,10,11) THEN 'Autumn'
-      ELSE 'Not exist' END
-      AS STATION_OF_YEAR
-    from
-    {{ source('demo', 'bike') }}
+    {{day_type('STARTED_AT')}} as DAY_OF_WEEK, -- Invoke a macro function
+    {{get_season('STARTED_AT')}} as STATION_OF_YEAR -- Invoke a macro function
+    --  {{function1('STARTED_AT')}} as TIME
+    
+    from {{ source('demo', 'bike') }}
 )
 
 select *
