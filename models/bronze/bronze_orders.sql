33eb5fd01fb5534b12fc42aa43519a6894117f2a
Fernando Fernando Lopez <FernandoLopez1975.FL@hotmail.com>
2026-06-01 21:48:08 +0000

    Added incremental materialization to bronze_orders

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -5,6 +5,43 @@
     )
 }}
 
+-- *** unique_key *** -> Configured DBT model to upsert rows into target (this or current model) based on same ID columns to avoid duplicated rows with same id.
+--
+-- /target/run/models/bronze/bronze_orders folder
+-- merge
+--    into
+--        `dbt_project_catalog`.`bronze`.`bronze_orders` as DBT_INTERNAL_DEST
+--    using
+--        `bronze_orders__dbt_tmp` as DBT_INTERNAL_SOURCE
+--    on      
+--              DBT_INTERNAL_SOURCE.`id` <=> DBT_INTERNAL_DEST.`id`   
+--    when matched
+--        then update set
+--            *
+--    when not matched
+--        then insert
+--            *
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'id'
+    )
+}}
+
 SELECT 
 *
 FROM {{ source('landing', 'orders') }}
+-- FROM {{ source('landing', 'orders_incremental') }}
+
+{# 
+
+To prevent duplicates rows in incremental load in DBT tables 
+Only be processed records that arrive into landing.orders_incremental after this date "max(created_at) from `dbt_project_catalog`.`bronze`.`bronze_orders`" based on created_at value
+
+*** this *** indicates current model, in this case bronze_orders.sql
+#}
+{% if is_incremental() %}
+
+where created_at > (select coalesce(max(created_at),'1900-01-01') from {{ this }}) 
+
+{% endif %}
